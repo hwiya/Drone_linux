aecb4b5e9754c3907c37bcb11ff2158487e7baf5
hwiya <hwijin0209@naver.com>
2018-07-06 14:56:13 +0900

    signal에 swich문 활용 update
    ctrl+c 5번하면 or ctrl+\ 5번하면 반복문 종료

--- process/ctrlc1.c
+++ process/ctrlc1.c
@@ -1,19 +1,32 @@
 #include <signal.h>
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 
 //SIGINT에 대응하는 핸들러
 void sigHandler(int sig)
 {
+	static int count = 0;
+	static int count1 = 0;
 	switch(sig)
 	{
 	case SIGINT:
 		printf("SIGINT %d\n",sig);
-		(void) signal(SIGINT, SIG_DFL);
+		
+		count++;
+		if(count == 5)
+		{
+			exit(0);
+		}
 		break;
 	case SIGQUIT:
 		printf("SIGQUIT %d\n",sig);
-		(void) signal(SIGQUIT, SIG_DFL);
+		
+		count1++;
+		if(count1 == 5)
+		{
+			exit(0);
+		}
 		break;
 	}
 }
