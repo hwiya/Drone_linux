a829c5eb0187610695645cdcb786c141851c6af6
hwiya <hwijin0209@naver.com>
2018-07-06 10:19:20 +0900

    fork의 if문 버전

--- process/getid.c
+++ process/getid.c
@@ -2,7 +2,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
 
+void pr_exit(int status);
 /*int delay_func(int delay)
 {
 	int i, result;
@@ -33,7 +36,9 @@ int main(void)
 	else if(pid == 0)
 	{
 		printf("Child : Hello!!\n");
-		return 0;
+		raise(SIGINT);
+		//exit(11);
+		//return 10;
 	}
 	//부모 프로세스인 경우 실행
 	else
@@ -46,5 +51,27 @@ int main(void)
 	pid = wait(&status);
 	printf("EXIT:Child pid = %d\n", pid);
 
+	pr_exit(status);
 	return 0;
+}
+
+void pr_exit(int status)
+{
+	//자식 프로세스가 정상적으로 종료했다면
+	if (WIFEXITED(status))
+	{	
+        	printf("normal termination, exit status = %d\n",\
+		WEXITSTATUS(status));
+	}	    
+	//자식 프로세스가 시그널에 의해 종료했다면
+	else if (WIFSIGNALED(status))
+	{	        
+		printf("abnormal termination, signal number = %d%s\n", \
+		WTERMSIG(status),   WCOREDUMP(status) ? "(core file generated)" : "");
+	}
+	//자식 프로세스가 정지된 상태라면
+	else if (WIFSTOPPED(status))
+	{	        
+		printf("child stopped, signal number = %d\n",   WSTOPSIG(status));
+	}
 }
