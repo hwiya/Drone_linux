6dba135a110de3003afa042052fcc271d3b7b10f
hwiya <hwijin0209@naver.com>
2018-07-06 14:48:29 +0900

    signal에 switch 사용

--- process/ctrlc1.c
+++ process/ctrlc1.c
@@ -5,12 +5,23 @@
 //SIGINT에 대응하는 핸들러
 void sigHandler(int sig)
 {
-	printf("OUCH! - i got signal %d\n",sig);
+	switch(sig)
+	{
+	case SIGINT:
+		printf("SIGINT %d\n",sig);
+		(void) signal(SIGINT, SIG_DFL);
+		break;
+	case SIGQUIT:
+		printf("SIGQUIT %d\n",sig);
+		(void) signal(SIGQUIT, SIG_DFL);
+		break;
+	}
 }
 
 int main()
 {
 	signal(SIGINT, sigHandler);
+	signal(SIGQUIT, sigHandler);
 
 	while(1)
 	{
