e5da83575215f1b150a4af8ba2cb4a22e8885cc4
hwiya <hwijin0209@naver.com>
2018-07-06 10:41:55 +0900

    other fork.c

---
+++ process/fork.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int glob = 6; /* external variable in initialized data */ 
+char buf[] = "a write to stdout\n"; 
+
+int main(void)
+{
+	int var; /* automatic variable on the stack */
+	pid_t pid; var = 88;
+
+	if (write(STDOUT_FILENO, buf, sizeof(buf)-1) != sizeof(buf)-1)
+	{
+		perror("write error");
+	}
+	
+	printf("before fork\n"); /* we don't flush stdout */ 
+	
+	if ( (pid = fork()) == 0)
+	{ 
+		/* child */
+		glob++; /* modify variables */ 
+		var++; 
+	} 
+	else
+	{
+		/* parent */ 
+		sleep(2);
+	} 
+	
+	printf("pid = %d, glob = %d, var = %d\n",getpid(),glob, var);
+	exit(0);
+} 
